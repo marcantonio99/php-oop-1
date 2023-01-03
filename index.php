f45db5e55402215d43c404106feeadedfaa1e387
marcantonio99 <marcantonio8799@gamil.com>
2023-01-03 12:41:05 +0100

    index

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-include_once __DIR__ . '/classes/movie.php';
+include_once __DIR__ . '/classi/movie.php';
+
+$movie1 = new Movie( 'The mask', 1994, 'Fantasy', 6 );
+
+echo $movie1->title;
+echo "<br>";
+echo $movie1->year;
+echo "<br>";
+echo $movie1->genre;
+echo "<br>";
+echo $movie1->age;
+echo "<br>";
+echo $movie1->perTutti();
+echo "<br>";
+
 
 ?>
